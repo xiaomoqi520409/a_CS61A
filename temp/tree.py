ff78b15b5f4990b129dc9b0dc71092bb3d48d917
xiaomoqi_zzq <2022467078@qq.com>
2025-06-19 21:00:47 +0800

    xiaomoqi

--- temp/tree.py
+++ temp/tree.py
@@ -38,3 +38,42 @@ def leaves(tree):
         return [label(tree)]
     else:
         return sum([leaves(b) for b in branches(tree)],[])  
+    
+def increment_leaves(t):
+    if is_leaf(t):
+        return tree(label(t)+1)
+    else:
+        bs=(increment_leaves(b) for b in branches(t))
+        return tree(label(t),bs)
+    
+def increment(t):
+    return tree(label(t)+1,[increment(b) for b in branches(t)])
+
+def print_tree(t,indent=0):
+    print(' '*indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b,indent+1)
+
+def fact_times(n,k=1):
+    if n==0:
+        return k
+    else:
+        return fact_times(n-1,k*n)
+    
+def fact(n):
+    return fact_times(n,1)
+
+def print_sums(t,so_far):
+    so_far=so_far+label(t)
+    if is_leaf(t):
+        print(so_far)
+    else:
+        for b in branches(t):
+            print_sums(b,so_far)
+
+def count_paths(t,total):
+    if label(t)==total:
+        found=1
+    else:
+        found=0
+    return found+sum([count_paths(b,total-label(t)) for b in branches(t)])
